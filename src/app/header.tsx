43db709385903313fc74b64aa3e1685fabd21939
Thomas <thomas@dillingdata.se>
2024-05-03 15:18:54 +0200

    Added divs to be used to hold the different parts of the header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,8 +2,10 @@
 
 export default function Header() {
     return(
-        <div className="h-20 w-full border-solid border-black border-4">
-            
+        <div className="h-20 w-full border-solid border-black border-4 grid grid-cols-3">
+            <div className="grid grid-2"></div>
+            <div className="grid grid-3"></div>
+            <div className="grid grid-2"></div>
         </div>
     )
 }
