d409175410c0fc6d0da24da95ceb0f06972bba4a
Thomas <thomas@dillingdata.se>
2024-05-03 15:29:07 +0200

    Added 4 link buttons that will be located in the middle of the header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,9 +3,18 @@
 export default function Header() {
     return(
         <div className="h-20 w-full border-solid border-black border-4 grid grid-cols-3">
-            <div className="grid grid-2"></div>
-            <div className="grid grid-3"></div>
-            <div className="grid grid-2"></div>
+            <div className="grid grid-cols-2">
+
+            </div>
+            <div className="grid grid-cols-3">
+                <div className="text-xl">Home</div>
+                <div className="text-xl">About</div>
+                <div className="text-xl">Portfolio</div>
+                <div className="text-xl">Contact</div>
+            </div>
+            <div className="grid grid-cols-2">
+
+            </div>
         </div>
     )
 }
