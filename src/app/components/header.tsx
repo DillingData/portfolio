28b98f1e0c4eab4667c683b60d7716622fe8975a
Thomas <thomas@dillingdata.se>
2024-05-03 21:32:15 +0200

    styling header buttons

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,23 +1,34 @@
 
 const textStyle = {
-    color: 'blue',
+    cursor: 'pointer',
 }
-
 
 export default function Header() {
     return(
-        <div className="h-20 w-full border-solid border-red-800 border-4 grid grid-cols-3" style={textStyle}>
+        <div className="h-20 w-full grid grid-cols-3 shadow-lg">
             <div className="grid grid-cols-2">
-
+                <div>{/* Add Logo Here */}</div>
+                <div></div>
             </div>
             <div className="grid grid-cols-4">
-                <div className="text-xl">Home</div>
-                <div className="text-xl">About</div>
-                <div className="text-xl">Portfolio</div>
-                <div className="text-xl">Contact</div>
+                <div className="text-xl flex justify-center items-center" >
+                    <p className="text-blue-500 hover:text-blue-700" style={textStyle}>
+                        Home
+                    </p>
+                </div>
+                <div className="text-xl flex justify-center items-center">
+                    <p className="text-blue-500 hover:text-blue-700" style={textStyle}>About</p>
+                </div>
+                <div className="text-xl flex justify-center items-center">
+                    <p className="text-blue-500 hover:text-blue-700" style={textStyle}>Portfolio</p>
+                </div>
+                <div className="text-xl flex justify-center items-center">
+                    <p className="text-blue-500 hover:text-blue-700" style={textStyle}>Contact</p>
+                </div>
             </div>
             <div className="grid grid-cols-2">
-
+                <div></div>
+                <div>{/* Add Another button (maybe dark mode) Here */}</div>
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/header";
 
+
 export default function Home() {
     return(
         <div className="bg-white h-screen">
