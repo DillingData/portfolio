de8cd50a54861dc977fe99e03f06b75ec5085a03
Thomas <thomas@dillingdata.se>
2024-05-03 15:55:46 +0200

    Moved header to components and corrected color so it can be seen

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,16 @@
 
+const textStyle = {
+    color: 'blue',
+}
+
 
 export default function Header() {
     return(
-        <div className="h-20 w-full border-solid border-black border-4 grid grid-cols-3">
+        <div className="h-20 w-full border-solid border-red-800 border-4 grid grid-cols-3" style={textStyle}>
             <div className="grid grid-cols-2">
 
             </div>
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-4">
                 <div className="text-xl">Home</div>
                 <div className="text-xl">About</div>
                 <div className="text-xl">Portfolio</div>
