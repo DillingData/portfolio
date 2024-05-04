92479142f092514a460d74440a75ebb698a274a9
Thomas <thomas@dillingdata.se>
2024-05-04 11:40:13 +0200

    added img source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header";
+import HomeBody from "./components/home";
 
 
 export default function Home() {
     return(
         <div className="bg-white h-screen">
             <Header />
+            <HomeBody />
         </div>
     )
 }
