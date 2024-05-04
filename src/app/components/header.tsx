cb4f9ad2535f6beedb7ef76b7a5a22ca44597e4a
Thomas <thomas@dillingdata.se>
2024-05-04 20:13:37 +0200

    Corrected header layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faH, faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const textStyle = {
     cursor: 'pointer',
@@ -12,26 +12,28 @@ export default function Header() {
                 <div>{/* Add Logo Here */}</div>
                 <div></div>
             </div>
-            <div className="grid grid-cols-5">
-                <div className="text-xl flex justify-end items-center" >
+            <div className="flex justify-center items-center">
+                <div className="text-xl m-5" >
                     <FontAwesomeIcon className='text-blue-500 hover:text-blue-700' icon={faHouse} width='20' />
                 </div>
+                {/* 
                 <div className="text-xl flex justify-center items-center" >
                     <p className="text-blue-500 hover:text-blue-700" style={textStyle}>
                         Home
                     </p>
                 </div>
-                <div className="text-xl flex justify-center items-center">
+                */}
+                <div className="text-xl m-5">
                     <p className="text-blue-500 hover:text-blue-700" style={textStyle}>
-                        Web
+                        About
                     </p>
                 </div>
-                <div className="text-xl flex justify-center items-center">
+                <div className="text-xl m-5">
                     <p className="text-blue-500 hover:text-blue-700" style={textStyle}>
                         Portfolio
                     </p>
                 </div>
-                <div className="text-xl flex justify-center items-center">
+                <div className="text-xl m-5">
                     <p className="text-blue-500 hover:text-blue-700" style={textStyle}>
                         Contact
                     </p>
