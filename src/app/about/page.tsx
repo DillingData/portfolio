2afc0b97fad2a6df67c903c53f45a6ac55ab2721
Thomas <thomas@dillingdata.se>
2024-05-06 14:54:05 +0200

    Fixing background being black in some places

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,51 +17,54 @@ export default function About() {
             <Header />
             <div className="text-[#011b45]"> 
                 <div className="mx-10">
-                    <p className="text-main-text text-center text-xl lg:text-3xl">I am a self-taught Developer, I have experience with the following technologies from 
+                    <p className="text-main-text text-center text-xl mt-5 lg:text-2xl">I am a self-taught Developer, I have experience with the following technologies from 
                         working at a major logistics company as a Full-Stack Developer</p>
                 </div>
-                <div className="bg-background-main mt-5 grid grid-cols-2 gap-5 lg:mt-24 lg:mx-10 lg:grid-cols-5 lg:gap-5 lg:pb-14">
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={JSLogo.src} alt="DillingData" width="172" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">JavaScript</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={ASPNET.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">ASP.NET</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={CSharp.src} alt="C# Logo" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">C#</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={SQL.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">Microsoft SQL</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={Tailwind.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">TailWind CSS</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={Bootstrap.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">BootStrap</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={ReactLogo.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text texst-md lg:text-3xl">ReactJS</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={NodeLogo.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">NodeJS</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="mx-auto mt-14 lg:mt-20" src={DaisyUI.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl mt-12">Daisy UI</p>
-                    </div>
-                    <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
-                        <img className="rounded-[50px] mx-auto lg:mt-5" src={WordPress.src} alt="DillingData" width="150" height="150" />
-                        <p className="text-main-text text-md lg:text-3xl">WordPress</p>
+                <div className="bg-white">
+                    <div className="bg-background-main mt-5 grid grid-cols-2 gap-5 lg:mt-24 lg:mx-10 lg:grid-cols-5 lg:gap-5 lg:pb-14">
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={JSLogo.src} alt="DillingData" width="172" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">JavaScript</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={ASPNET.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">ASP.NET</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={CSharp.src} alt="C# Logo" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">C#</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={SQL.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">Microsoft SQL</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={Tailwind.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">TailWind CSS</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={Bootstrap.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">BootStrap</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={ReactLogo.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text texst-md lg:text-3xl">ReactJS</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={NodeLogo.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">NodeJS</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="mx-auto mt-14 lg:mt-20" src={DaisyUI.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl mt-12">Daisy UI</p>
+                        </div>
+                        <div className="bg-[#f2f2f2] w-[180px] h-[180px] mx-auto lg:w-[230px] lg:h-[230px] rounded-[50px] text-center">
+                            <img className="rounded-[50px] mx-auto lg:mt-5" src={WordPress.src} alt="DillingData" width="150" height="150" />
+                            <p className="text-main-text text-md lg:text-3xl">WordPress</p>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
