3cbe7481985c06bb1e2eeb3b5e38290f62d0f0fb
Thomas <thomas@dillingdata.se>
2024-05-07 21:50:07 +0200

    Added one portfolio project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/header";
 import HomeBody from "./components/home";
-import Link from "next/link";
 
 
 export default function Home() {
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,29 @@
  import React from "react";
  import Header from "../components/header";
+ import skaan from "../../img/Skanemorskan.png"
 
  export default function portfolio() {
     return(
         <div className="bg-white h-screen">
             <Header />
             <div className="text-[#011b45]">
-                <p>Portfolio</p>
+                <p></p>
+
+                <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <a href="https://www.skanemorskan.se" target="_blank">
+                        <img className="w-full" src={skaan.src} alt="Sunset in the mountains" />
+                    </a>
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">SkåneMorskan</div>
+                        <p className="text-gray-700 text-base">
+                            Presentation of the webPage
+                        </p>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#nursing</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#birth preperation</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
