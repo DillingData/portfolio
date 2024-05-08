4e0ddd8defb8b1b7bd6f2a8bd543db909368ceae
Thomas <thomas@dillingdata.se>
2024-05-08 07:06:10 +0200

    Corrected 'tags' for project on portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -51,8 +51,8 @@
                                 </p>
                             </div>
                             <div className="px-6 pt-4 pb-2">
-                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Logistics</span>
-                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#reports </span>
+                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#logistics</span>
+                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#planning</span>
                             </div>
                         </div>
                     </div>
